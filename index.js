5e2327cc39c754dbb8aa132c45bcf4413d5a7b55
Sharifa Islam <sislam6@buffalo.edu>
2022-07-04 11:31:34 -0400

    commit tree 36b69badfb92af0052d6b9191435ea74548649b0

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function introduction(name) {
     return `Hi, my name is ${name}.`;
   }
 function introductionWithLanguage(name,language) {
-  console.log(`Hi,my name is ${name} and I am learning to program in ${language}.`) 
+  console.log(`Hi,my name is ${name} and I am learning to program in ${language}.`)
   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
-  }  
+  }
 function introductionWithLanguageOptional(name,language = "JavaScript") {
   console.log(`Hi,my name is ${name} and I am learning to program in ${language}.`)
   return `Hi, my name is ${name} and I am learning to program in ${language}.`
-  }
   
+
 
 
 
